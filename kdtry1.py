6d2a4c15129ce0e0644695833271d332d44c815a
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-09-05 01:17:28 +0530

    Update kdtry1.py

--- kdtry1.py
+++ kdtry1.py
@@ -6,7 +6,7 @@ import wandb
 # Initialize a new W&B run
 wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775")
 # Initialize wandb
-wandb.init(project="yolov8-LDConv", config=opt.__dict__)
+wandb.init(project="yolov8-LDConv")
 
 # Load the custom model configuration
 model = YOLO('yolov8-LDconv.yaml')
