c6c3e401c508c48fcdff36fb34f430517e0e90f5
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-09-05 04:22:14 +0530

    Update kdtry1.py

--- kdtry1.py
+++ kdtry1.py
@@ -39,7 +39,7 @@ model.add_callback('on_train_batch_end', log_losses)
 Result_Final_model = model.train(
     data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
     epochs=3,
-    batch=16,
+    batch=8,
     optimizer='auto',
     project='yolov8-LDConv',
     save=True,
