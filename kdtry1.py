9dba0ce9623723861626616b4314eb7f49f836ce
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-09-05 01:53:39 +0530

    Update kdtry1.py

--- kdtry1.py
+++ kdtry1.py
@@ -14,28 +14,10 @@ wandb.init(project="yolov8-LDConv")
 model = YOLO('yolov8-LDconv.yaml')
 
 # Load the pretrained model from the provided path
-# Kaggle typically mounts the dataset under '/kaggle/input/'
-pretrained_model = YOLO('/kaggle/input/yolov8m-pt/yolov8m.pt')  # Adjust the path if necessary
-
-# Extract the state_dict (the actual model weights) from the pretrained model
-pretrained_state_dict = pretrained_model['model'].state_dict()
-
-# Get the current model's state_dict
-model_state_dict = model.model.state_dict()
-
-# Create a new state_dict that will only contain weights for common layers
-new_state_dict = OrderedDict()
-
-# Match layers and load only common weights
-for layer_name in model_state_dict.keys():
-    if layer_name in pretrained_state_dict and model_state_dict[layer_name].shape == pretrained_state_dict[layer_name].shape:
-        new_state_dict[layer_name] = pretrained_state_dict[layer_name]
-    else:
-        # If a layer is not common, use the initialized weights of the current model
-        new_state_dict[layer_name] = model_state_dict[layer_name]
+model_state_dict = torch.load('/kaggle/input/yolov8m-pt/yolov8m.pt')  
 
 # Load the filtered state_dict into your model
-model.model.load_state_dict(new_state_dict)
+model.model.load_state_dict(model_state_dict,strict = False)
 model.model.to(device)
 
 # Train the model with the specified configuration and sync to W&B
