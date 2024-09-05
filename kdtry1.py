e607a34258940fa17b26e0a4dd2e55fd0222b376
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-09-06 01:25:07 +0530

    Update kdtry1.py

--- kdtry1.py
+++ kdtry1.py
@@ -41,7 +41,7 @@ model.add_callback('on_train_batch_end', log_losses)
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
     data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
-    epochs=50,
+    epochs=30,
     batch=8,
     optimizer='auto',
     project='yolov8-LDConv',
