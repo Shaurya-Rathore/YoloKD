2eeba2c8fbbb4e1a4b8e9129d8d05ef6efb701eb
Shaurya Rathore <140714862+Wackyjac@users.noreply.github.com>
2024-09-07 11:57:33 +0530

    fixed??

--- kdtry1.py
+++ kdtry1.py
@@ -4,8 +4,8 @@ import wandb
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # Initialize W&B
-wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
-wandb.init(project="yolov8-LDConv")
+# wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
+# wandb.init(project="yolov8-LDConv")
 
 # Load the custom model configuration
 model = YOLO('yolov8-LDconv.yaml')
@@ -39,11 +39,11 @@ def log_losses(trainer):
     if torch.isnan(loss_items[2]) or torch.isinf(loss_items[2]):
         print("Warning: DFL loss contains NaN or inf values")
     
-    wandb.log({
-        "train/box_loss": loss_items[0],
-        "train/cls_loss": loss_items[1],
-        "train/dfl_loss": loss_items[2]
-    }, step=trainer.epoch)
+    # wandb.log({
+    #     "train/box_loss": loss_items[0],
+    #     "train/cls_loss": loss_items[1],
+    #     "train/dfl_loss": loss_items[2]
+    # }, step=trainer.epoch)
     torch.cuda.empty_cache()
 
 # Register the callback with the YOLO model
@@ -63,8 +63,4 @@ Result_Final_model = model.train(
 torch.save(model.model.state_dict(), '/kaggle/working/yolov8m_custom_weights.pt')
 
 # Finish W&B run
-wandb.finish()
-
-
-
-
+# wandb.finish()
