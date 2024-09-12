3f6a66afa71dda45baa1264f97b40ef825223110
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-09-12 23:57:31 +0530

    Update kdtry1.py

--- kdtry1.py
+++ kdtry1.py
@@ -51,7 +51,7 @@ model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with a reduced batch size
 Result_Final_model = model.train(
-    data='/kaggle/input/ooga-dataset/ooga/ooga-main/ooga/data.yaml',
+    data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
     epochs=50,
     batch=8,
     optimizer='auto',
