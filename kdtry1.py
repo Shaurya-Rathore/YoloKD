a6146b4c48dd5516b28773d369b763186af272e0
Shaurya Rathore <140714862+Wackyjac@users.noreply.github.com>
2024-09-12 18:33:12 +0530

    homing in on the line

--- kdtry1.py
+++ kdtry1.py
@@ -51,7 +51,7 @@ model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with a reduced batch size
 Result_Final_model = model.train(
-    data='/kaggle/input/oogadataset/ooga/ooga-main/ooga/data.yaml',
+    data='/kaggle/input/ooga-dataset/ooga/ooga-main/ooga/data.yaml',
     epochs=50,
     batch=8,
     optimizer='auto',
