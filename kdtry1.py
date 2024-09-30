92e12c9f4cc14b7d944c9417c60b3a28d44317a5
Shaurya Rathore <140714862+Wackyjac@users.noreply.github.com>
2024-09-30 18:43:51 +0530

    before remote

--- kdtry1.py
+++ kdtry1.py
@@ -52,8 +52,8 @@ model.add_callback('on_train_batch_end', log_losses)
 # Train the model with a reduced batch size
 Result_Final_model = model.train(
     data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
-    warmup_epochs = 0,
-    epochs=3,
+    warmup_epochs = 2,
+    epochs=30,
     batch=8,
     optimizer='auto',
     project='yolov8',
