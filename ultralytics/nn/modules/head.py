e78db0bc138e10f25cc876fe3891e6c1a7b9a4c4
Hem Gosalia <149922474+HemGosalia@users.noreply.github.com>
2024-10-04 21:10:18 +0530

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 from torch.nn.init import constant_, xavier_uniform_
 
-from ultralytics.nn.modules.tal import TORCH_1_10, dist2bbox, dist2rbox, make_anchors
+from ultralytics.utils.tal import TORCH_1_10, dist2bbox, dist2rbox, make_anchors
 
 from .block import DFL, BNContrastiveHead, ContrastiveHead, Proto
 from .conv import Conv
