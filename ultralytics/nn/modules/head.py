683bf0d77e420a063ee5dc97b90ae7b4fcbb6cd0
Hem Gosalia <149922474+xxHeisenberg31xx@users.noreply.github.com>
2024-10-04 21:01:25 +0530

    summary

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 from torch.nn.init import constant_, xavier_uniform_
 
-from ultralytics.utils.tal import TORCH_1_10, dist2bbox, dist2rbox, make_anchors
+from ultralytics.nn.modules.tal import TORCH_1_10, dist2bbox, dist2rbox, make_anchors
 
 from .block import DFL, BNContrastiveHead, ContrastiveHead, Proto
 from .conv import Conv
@@ -16,7 +16,6 @@ from .transformer import MLP, DeformableTransformerDecoder, DeformableTransforme
 from .utils import bias_init_with_prob, linear_init
 
 __all__ = "Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder", "v10Detect"
-
 
 class Detect(nn.Module):
     """YOLOv8 Detect head for detection models."""
@@ -588,3 +587,41 @@ class v10Detect(Detect):
             for x in ch
         )
         self.one2one_cv3 = copy.deepcopy(self.cv3)
+
+
+from torchsummary import summary
+
+# Assuming Detect class is already defined, as provided in your code
+class Conv(nn.Module):
+    def __init__(self, c_in, c_out, kernel_size, stride=1, padding=1):
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c_in, c_out, kernel_size, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(c_out)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
+
+class DFL(nn.Module):
+    def __init__(self, reg_max):
+        super(DFL, self).__init__()
+        self.fc = nn.Linear(reg_max, reg_max)
+
+    def forward(self, x):
+        return self.fc(x)
+
+# Define number of classes and channels for testing
+nc = 80  # Number of classes
+ch = [256, 512, 1024]  # List of channels for different detection levels
+
+# Instantiate the Detect class
+detect_model = Detect(nc=nc, ch=ch)
+
+# Move model to the appropriate device (CPU in this case)
+device = torch.device("cpu")
+detect_model.to(device)
+
+# Print summary of the model to see the number of parameters
+# Use a dummy input size for each detection level
+input_size = [(256, 150, 150), (512, 75, 75), (1024, 38, 38)]  # List of input feature map sizes
+summary(detect_model, input_size=input_size, device=str(device))
