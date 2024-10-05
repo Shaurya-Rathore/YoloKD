5124a03d186190e72d57e9016771622a5409950b
Hem Gosalia <149922474+xxHeisenberg31xx@users.noreply.github.com>
2024-10-05 19:06:05 +0530

    testing head

--- ultralytics/nn/modules/model_search.py
+++ ultralytics/nn/modules/model_search.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import unittest
 import copy
+from ultralytics.utils.tal import dist2bbox, make_anchors
 from ultralytics.nn.modules.conv import Conv
 from ultralytics.nn.modules.block import DFL
-from ultralytics.utils.tal import dist2bbox, make_anchors
 from operations import *
 from torch.autograd import Variable
 
