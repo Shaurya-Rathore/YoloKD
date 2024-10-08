c6360069bd378fec09c002c645afb59f3d6388e6
adityax1412 <adityanaidu2004@gmail.com>
2024-10-08 18:44:48 +0530

    revert

--- ultralytics/nn/modules/model_search.py
+++ ultralytics/nn/modules/model_search.py
@@ -86,7 +86,7 @@ class DFL(nn.Module):
         return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
         # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
 
-class MixedOp(nn.Module):
+"""class MixedOp(nn.Module):
 
   def __init__(self, C, stride):
     super(MixedOp, self).__init__()
@@ -116,7 +116,20 @@ class MixedOp(nn.Module):
       
       result += w * out
 
-    return result
+    return result"""
+class MixedOp(nn.Module):
+
+  def __init__(self, C, stride):
+    super(MixedOp, self).__init__()
+    self._ops = nn.ModuleList()
+    for primitive in PRIMITIVES:
+      op = OPS[primitive](C, stride, False)
+      if 'pool' in primitive:
+        op = nn.Sequential(op, nn.BatchNorm2d(C, affine=False))
+      self._ops.append(op)
+
+  def forward(self, x, weights):
+    return sum(w * op(x) for w, op in zip(weights, self._ops))
 
 class StemLayer(nn.Module):
   def __init__(self, in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1):
@@ -131,7 +144,7 @@ class StemLayer(nn.Module):
     x = self.relu(x)
     return x
 
-class Cell(nn.Module):
+"""class Cell(nn.Module):
 
   def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev):
     super(Cell, self).__init__()
@@ -183,7 +196,42 @@ class Cell(nn.Module):
     final_output = torch.cat(states[-self._multiplier:], dim=1)
     
     print(f"Final concatenated state shape: {final_output.shape}")
-    return final_output
+    return final_output"""
+
+class Cell(nn.Module):
+
+  def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev):
+    super(Cell, self).__init__()
+    self.reduction = reduction
+
+    if reduction_prev:
+      self.preprocess0 = FactorizedReduce(C_prev_prev, C, affine=False)
+    else:
+      self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0, affine=False)
+    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
+    self._steps = steps
+    self._multiplier = multiplier
+
+    self._ops = nn.ModuleList()
+    self._bns = nn.ModuleList()
+    for i in range(self._steps):
+      for j in range(2+i):
+        stride = 2 if reduction and j < 2 else 1
+        op = MixedOp(C, stride)
+        self._ops.append(op)
+
+  def forward(self, s0, s1, weights):
+    s0 = self.preprocess0(s0)
+    s1 = self.preprocess1(s1)
+
+    states = [s0, s1]
+    offset = 0
+    for i in range(self._steps):
+      s = sum(self._ops[offset+j](h, weights[offset+j]) for j, h in enumerate(states))
+      offset += len(states)
+      states.append(s)
+
+    return torch.cat(states[-self._multiplier:], dim=1)
 
 class Network(nn.Module):
 
