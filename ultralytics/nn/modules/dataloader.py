5529cc2e33c4e6126c5935a235370bd183c75c6b
adityax1412 <adityanaidu2004@gmail.com>
2024-10-23 18:31:54 +0530

    fn

--- ultralytics/nn/modules/dataloader.py
+++ ultralytics/nn/modules/dataloader.py
@@ -54,10 +54,10 @@ class YOLOObjectDetectionDataset(Dataset):
         labels = torch.tensor(labels)
 
         # Create target tensors
-        boxes = labels[:, 1:] if len(labels) > 0 else torch.zeros((0, 4))
-        labels = labels[:, 0].long() if len(labels) > 0 else torch.zeros(0, dtype=torch.int64)
+        #boxes = labels[:, 1:] if len(labels) > 0 else torch.zeros((0, 4))
+        #labels = labels[:, 0].long() if len(labels) > 0 else torch.zeros(0, dtype=torch.int64)
 
-        return image, boxes, labels
+        return image, labels
 
     def get_class_name(self, class_id):
         return self.classes[class_id]
@@ -67,8 +67,10 @@ def custom_collate_fn(batch):
     images = []
     targets = []
 
-    for i, (image, box,label) in enumerate(batch):
+    for i, (image,labels) in enumerate(batch):
         images.append(image)
+        box = labels[:, 1:] if len(labels) > 0 else torch.zeros((0, 4))
+        label = labels[:, 0].long() if len(labels) > 0 else torch.zeros(0, dtype=torch.int64)
 
         if box.numel() > 0:  # Check if there are any boxes
             # Calculate the center coordinates, width, and height for each bounding box
