1dc92a3ab601912fcda13b47450fd878c9b9e8bc
adityax1412 <adityanaidu2004@gmail.com>
2024-10-27 15:10:55 +0530

    sant

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -198,8 +198,9 @@ class v8DetectionLoss:
     def bbox_decode(self, anchor_points, pred_dist):
         """Decode predicted object bounding box coordinates from anchor points and distribution."""
         if self.use_dfl:
-            b, a, c = pred_dist.shape  # batch, anchors, channels
-            pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(self.proj.type(pred_dist.dtype))
+            b, a, c = pred_dist.shape
+            proj = self.proj.to(pred_dist.device)  # Move proj to pred_dist's device
+            pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(proj.type(pred_dist.dtype))
             # pred_dist = pred_dist.view(b, a, c // 4, 4).transpose(2,3).softmax(3).matmul(self.proj.type(pred_dist.dtype))
             # pred_dist = (pred_dist.view(b, a, c // 4, 4).softmax(2) * self.proj.type(pred_dist.dtype).view(1, 1, -1, 1)).sum(2)
         return dist2bbox(pred_dist, anchor_points, xywh=False)
