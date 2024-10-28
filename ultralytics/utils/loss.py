4833fcc640c43a8827fac5f39df301ee7394863f
adityax1412 <adityanaidu2004@gmail.com>
2024-10-28 18:05:01 +0530

    day

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -204,6 +204,7 @@ class v8DetectionLoss:
             pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(proj.type(pred_dist.dtype))
             # pred_dist = pred_dist.view(b, a, c // 4, 4).transpose(2,3).softmax(3).matmul(self.proj.type(pred_dist.dtype))
             # pred_dist = (pred_dist.view(b, a, c // 4, 4).softmax(2) * self.proj.type(pred_dist.dtype).view(1, 1, -1, 1)).sum(2)
+            anchor_points = anchor_points.to(self.device)
         return dist2bbox(pred_dist, anchor_points, xywh=False)
 
     def __call__(self, preds, batch):
