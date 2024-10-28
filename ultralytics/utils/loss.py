d30ddc641f6341a68cabfef715d77c2727672f65
adityax1412 <adityanaidu2004@gmail.com>
2024-10-28 18:02:53 +0530

    day

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -199,6 +199,7 @@ class v8DetectionLoss:
         """Decode predicted object bounding box coordinates from anchor points and distribution."""
         if self.use_dfl:
             b, a, c = pred_dist.shape
+            pred_dist = pred_dist.to(self.device)
             proj = self.proj.to(self.device)  # Move proj to pred_dist's device
             pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(proj.type(pred_dist.dtype))
             # pred_dist = pred_dist.view(b, a, c // 4, 4).transpose(2,3).softmax(3).matmul(self.proj.type(pred_dist.dtype))
