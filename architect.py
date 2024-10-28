db652b7b44d8ecf548690685d4f4fa7140dd401d
adityax1412 <adityanaidu2004@gmail.com>
2024-10-28 23:19:26 +0530

    pop

--- architect.py
+++ architect.py
@@ -39,7 +39,8 @@ class Architect(object):
 
   def _backward_step(self, input_valid, target_valid):
     with autocast():
-      loss,lossitem = self.model._loss(input_valid, target_valid)
+      loss= self.model._loss(input_valid, target_valid)
+      print (loss)
       loss.backward()
 
   def _backward_step_unrolled(self, input_train,target_train, input_valid, target_valid, eta, network_optimizer):
