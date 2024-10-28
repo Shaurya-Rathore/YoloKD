9042be50b42c9dc77d85ff5bc91c5c1a42cf254f
adityax1412 <adityanaidu2004@gmail.com>
2024-10-28 18:24:10 +0530

    day

--- ultralytics/utils/tal.py
+++ ultralytics/utils/tal.py
@@ -105,6 +105,7 @@ class TaskAlignedAssigner(nn.Module):
         mask_gt = mask_gt.bool()  # b, max_num_obj, h*w
         overlaps = torch.zeros([self.bs, self.n_max_boxes, na], dtype=pd_bboxes.dtype, device=pd_bboxes.device)
         bbox_scores = torch.zeros([self.bs, self.n_max_boxes, na], dtype=pd_scores.dtype, device=pd_scores.device)
+        print(f'pd : {pd_bboxes.device} scores: {pd_scores.device}')
 
         ind = torch.zeros([2, self.bs, self.n_max_boxes], dtype=torch.long)  # 2, b, max_num_obj
         ind[0] = torch.arange(end=self.bs).view(-1, 1).expand(-1, self.n_max_boxes)  # b, max_num_obj
