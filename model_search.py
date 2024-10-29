114caac5586689380871cb0cc43158632c364872
adityax1412 <adityanaidu2004@gmail.com>
2024-10-29 22:37:23 +0530

    3peg

--- model_search.py
+++ model_search.py
@@ -462,9 +462,9 @@ class Detect(nn.Module):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
         if self.end2end:
             return self.forward_end2end(x)
-
-        for i in range(self.nl):
-            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        with autocast:
+            for i in range(self.nl):
+                x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
         #if self.training:  # Training path
             #return x
         y = self._inference(x)
