957f6179ffcfc58f2799e7fc27742526a5f10e5f
adityax1412 <adityanaidu2004@gmail.com>
2024-10-29 23:02:36 +0530

    tr

--- model_search.py
+++ model_search.py
@@ -462,8 +462,9 @@ class Detect(nn.Module):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
         if self.end2end:
             return self.forward_end2end(x)
-        x[i] = x[i].float()
+        
         for i in range(self.nl):
+            x[i] = x[i].float()
             x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
         #if self.training:  # Training path
             #return x
