1c0b959428dfba3e0262239ac32144a8c4836df2
Shaurya Rathore <140714862+Wackyjac@users.noreply.github.com>
2024-11-04 18:50:44 +0530

    lossing

--- trainer.py
+++ trainer.py
@@ -338,6 +338,7 @@ def train(train_queue, model, teacher, criterion, optimizer, args):
             output = output[0]
             print(f'trainers {output}')
             output = output.unsqueeze(0)
+            print(f'the input to postprocess {output.shape}')
             output = layer_teacher.postprocess(output.permute(0, 2, 1), 100, 6)
             print(f'postprocess {output}')
 
