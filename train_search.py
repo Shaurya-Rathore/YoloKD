a638ef8b8cdbe3eeab005b9223e927e4d7d96ad0
adityax1412 <adityanaidu2004@gmail.com>
2024-11-06 19:52:20 +0530

    prints

--- train_search.py
+++ train_search.py
@@ -189,7 +189,7 @@ def main():
                 lr
             )
         for i in predforprint:
-            print(i)
+            logging.info('burrr',i)
         logging.info(
                 'Training - Total Loss: %.4f | Box Loss: %.4f | Class Loss: %.4f | DFL Loss: %.4f',
                 train_total_loss,
