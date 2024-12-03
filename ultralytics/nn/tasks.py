9b4a56c47be9f5cdd959e7483eb2caaca1ed6a47
adityax1412 <adityanaidu2004@gmail.com>
2024-12-03 19:21:12 +0530

    we back boys

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1006,7 +1006,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 else:
                     c2 = ch[f]
 
-        m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
+                m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
         t = str(m)[8:-2].replace("__main__.", "")  # module type
         m.np = sum(x.numel() for x in m_.parameters())  # number params
         m_.i, m_.f, m_.type = i, f, t  # attach index, 'from' index, type
