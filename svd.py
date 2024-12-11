e91a003f1d3acd3674b5278e00fa46725ecf4d25
adityax1412 <adityanaidu2004@gmail.com>
2024-12-11 17:16:53 +0530

    batch

--- svd.py
+++ svd.py
@@ -1,6 +1,5 @@
 from ultralytics.models.yolo import YOLO
 import yaml
-from ultralytics.cfg import v8
 # Load the YAML file
 with open('yolov8-softshare.yaml', 'r') as file:
     config = yaml.safe_load(file)
