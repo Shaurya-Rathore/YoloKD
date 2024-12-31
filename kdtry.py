01fcba96306c7451957e4ff00e1e04b1fa4ff171
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-12-31 15:05:34 +0530

    Update kdtry.py

--- kdtry.py
+++ kdtry.py
@@ -10,7 +10,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
 wandb.init(project="yolov8")
 # Load the custom model configuration
-model = YOLO('yolov8m.yaml')
+model = YOLO('yolov8n.yaml')
 model.model.to(device)
 
 # Define a callback to log losses at the end of each training batch
