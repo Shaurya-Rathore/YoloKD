a37b72131e7db9306a1e30600caa021e2be3ab03
adityax1412 <adityanaidu2004@gmail.com>
2025-01-06 18:24:31 +0530

    save

--- kdtry.py
+++ kdtry.py
@@ -33,7 +33,7 @@ model.add_callback('on_train_batch_end', log_losses)
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
     data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
-    epochs=1,
+    epochs=40,
     batch=8,
     optimizer='SOAP',
     project='yolov8_softshare',
