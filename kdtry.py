69e6c115a5518a3e9d3d68e4e8b180e56de24ded
adityax1412 <adityanaidu2004@gmail.com>
2025-01-09 17:48:27 +0530

    ehhe

--- kdtry.py
+++ kdtry.py
@@ -10,7 +10,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
 wandb.init(project="yolov8_softshare")
 # Load the custom model configuration
-model = YOLO('yolov8n-softshare.yaml')
+model = YOLO('yolov8n.yaml')
 model.model.to(device)
 
 # Define a callback to log losses at the end of each training batch
@@ -33,7 +33,7 @@ model.add_callback('on_train_batch_end', log_losses)
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
     data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
-    epochs=40,
+    epochs=70,
     batch=8,
     optimizer='SOAP',
     project='yolov8_softshare',
