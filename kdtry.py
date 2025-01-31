24728dd1fe8b20e3f624add8f4470975ad33b343
Hem Gosalia <149922474+HemGosalia@users.noreply.github.com>
2025-01-31 18:09:51 +0530

    Update kdtry.py

--- kdtry.py
+++ kdtry.py
@@ -10,7 +10,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 wandb.init(project="yolo_buck_patched_benchmarks")
 
 # Load the custom model configuration
-model = YOLO('yolov9t.yaml')
+model = YOLO('yolov5n.yaml')
 model.model.to(device)
 
 # Define a callback to log losses at the end of each training batch
@@ -43,7 +43,7 @@ Result_Final_model = model.train(
 )
 
 # Define model and dataset names
-model_name = "yolov9t_vanilla"
+model_name = "yolov5n_vanilla"
 dataset_name = "bucktales-patched"
 
 # Save the model as .pth file in Kaggle workspace
