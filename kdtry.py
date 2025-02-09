97bb7a3a1a1e72b34ee53269fbc24f212eda25f3
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2025-02-09 18:13:34 +0530

    Update kdtry.py

--- kdtry.py
+++ kdtry.py
@@ -34,7 +34,7 @@ model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
-    data="/content/drive/MyDrive/bucktales/bucktales_patched/dtc2023.yaml",
+    data="/content/drive/MyDrive/bucktales/bucktales_patched/dtc2023_RTDETR_Colab.yaml",
     epochs=70,
     batch=8,
     optimizer='auto',
